308d89cd502faa3207e753c653e0190c2f73cfeb
BharathMKulkarni <bharathmkulkarni@gmail.com>
2022-08-12 00:55:22 +0530

    removed redundant code

--- src/main/java/com/example/BorneoAssignment/Controller/DriveController.java
+++ src/main/java/com/example/BorneoAssignment/Controller/DriveController.java
@@ -26,8 +26,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.xml.sax.SAXException;
-
 import javax.annotation.PostConstruct;
+import javax.jdo.annotations.Value;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.util.ArrayList;
@@ -40,39 +40,37 @@ public class DriveController {
     private static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
     private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
     private static List<String> SCOPES = Collections.singletonList(DriveScopes.DRIVE);
-
     private static final String USER_IDENTIFIER_KEY = "MY_DUMMY_USER3";
-
-    //    @Value("${google.oauth.callback.uri}")
     private String CALLBACK_URI = "https://127.0.0.1.nip.io:8443/oauth";
-
-    //    @Value("${google.secret.key.path}")
     private String gdSecretKeys = "/home/bharathmkulkarni/BHARATH/Academics/Programming/Borneo/BorneoAssignment/src/main/resources/Keys/credentials.json";
-
-    //    @Value("${google.credentials.folder.path}")
     private String credentialsFolder = "/home/bharathmkulkarni/BHARATH/Academics/Programming/Borneo/BorneoAssignment/src/main/resources/CrediantialsStorage";
-
     private GoogleAuthorizationCodeFlow flow;
+    private Credential credentials ;
+    private Drive service ;
 
     public DriveController() throws FileNotFoundException {
     }
 
     @PostConstruct
     public void init() throws IOException {
-
-        GoogleClientSecrets secrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(
-                new FileInputStream(gdSecretKeys)
-        ));
+        GoogleClientSecrets secrets = GoogleClientSecrets.load(
+                JSON_FACTORY, new InputStreamReader(
+                        new FileInputStream(gdSecretKeys)
+                ));
 
         flow = new GoogleAuthorizationCodeFlow.Builder(
                 HTTP_TRANSPORT, JSON_FACTORY, secrets, SCOPES)
                 .setDataStoreFactory(new FileDataStoreFactory(new File(credentialsFolder)))
                 .build();
+
+        credentials = flow.loadCredential(USER_IDENTIFIER_KEY);
+
+        service = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, credentials)
+                .setApplicationName("BorneoAssignmentApplication").build();
     }
 
     @GetMapping("/")
     public String showHomePage() throws IOException {
-
         boolean isUserAuthenticated = false;
 
         Credential credential = flow.loadCredential(USER_IDENTIFIER_KEY);
@@ -88,7 +86,6 @@ public class DriveController {
 
     @GetMapping("/googlesignin")
     public void doGoogleSignIn(HttpServletResponse response) throws IOException {
-
         GoogleAuthorizationCodeRequestUrl url = flow.newAuthorizationUrl();
         String redirectURL = url.setRedirectUri(CALLBACK_URI).setAccessType("offline").build();
 
@@ -97,7 +94,6 @@ public class DriveController {
 
     @GetMapping("/oauth")
     public String saveAuthorizationCode(@RequestParam String code) throws IOException {
-
         if (code != null) {
             saveToken(code);
             return "dashboard.html";
@@ -115,22 +111,13 @@ public class DriveController {
     @GetMapping(value = {"/listFiles"}, produces = {"application/json"})
     public @ResponseBody List<FileItemDTO> listFiles() throws IOException {
 
-        Credential credentials = flow.loadCredential(USER_IDENTIFIER_KEY);
-
-        // Build a new authorized API client service.
-        Drive service = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, credentials)
-                .setApplicationName("BorneoAssignmentApplication").build();
-
-        List<com.google.api.services.drive.model.File> files = new ArrayList<>();
         List<FileItemDTO> resultList = new ArrayList<>();
-        String pageToken = null;
 
         FileList result = service.files().list()
                 .setFields("*")
                 .setSpaces("drive")
-                .setFields("nextPageToken, files(id, name)")
+                .setFields("files(id, name)")
                 .execute();
-
 
         for (com.google.api.services.drive.model.File file : result.getFiles()) {
             FileItemDTO fileItemDTO = new FileItemDTO();
@@ -139,12 +126,7 @@ public class DriveController {
             fileItemDTO.setName(file.getName());
 
             resultList.add(fileItemDTO);
-
         }
-
-        files.addAll(result.getFiles());
-
-        pageToken = result.getNextPageToken();
 
         return resultList;
     }
@@ -152,17 +134,7 @@ public class DriveController {
     @GetMapping(value = {"/search"}, produces = {"application/json"})
     public @ResponseBody List<FileItemDTO> searchFilesWithQuery(@RequestParam("q") String q) throws IOException, TikaException, SAXException {
 
-        Credential credentials = flow.loadCredential(USER_IDENTIFIER_KEY);
-
-        Drive service = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, credentials)
-                .setApplicationName("BorneoAssignmentApplication").build();
-
         List<FileItemDTO> resultList = new ArrayList<>();
-
-        // BUILD THE SEARCH QUERY
-        // NOT REQUIRED , TO BE REMOVED
-        String searchQuery = "fullText contains " + "'" + q + "'";
-        System.out.println("searchQuery: " + searchQuery);
 
         FileList result = service.files().list()
                 .setFields("*")
@@ -190,7 +162,6 @@ public class DriveController {
     }
 
 
-    FileOutputStream fileOutputStream = new FileOutputStream("temp.txt");
 
     private boolean searchString(String id, String q) throws IOException, GoogleJsonResponseException, TikaException, SAXException {
 
